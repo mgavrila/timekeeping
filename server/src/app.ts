af9ff2df8b9b23e670a5a21b75e75159fe6b16b4
Andrei Gavrila <andrei.gavrila94@yahoo.com>
2022-11-21 15:00:59 +0200

    Added user/auth model, schema, controllers and services

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,33 +5,70 @@ import morgan from "morgan";
 import cors from "cors";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import connectDB from "./utils/connectDB";
+import cookieParser from "cookie-parser";
 import redisClient from "./utils/connectRedis";
-
+import { createUserSchema, loginUserSchema } from "./schemas/user.schema";
+import {
+  loginHandler,
+  logoutHandler,
+  refreshAccessTokenHandler,
+  registerHandler,
+} from "./controllers/auth.controller";
 import customConfig from "./config/default";
-import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+import { inferAsyncReturnType, initTRPC, TRPCError } from "@trpc/server";
+import { deserializeUser } from "./middleware/deserializeUser";
+import { getMeHandler } from "./controllers/user.controller";
 
 dotenv.config({ path: path.join(__dirname, "./.env") });
 
-const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) =>
+  deserializeUser({ req, res });
 
 export type Context = inferAsyncReturnType<typeof createContext>;
 
 const t = initTRPC.context<Context>().create();
 
-const appRouter = t.router({
+const authRouter = t.router({
+  registerUser: t.procedure
+    .input(createUserSchema)
+    .mutation(({ input }) => registerHandler({ input })),
+  loginUser: t.procedure
+    .input(loginUserSchema)
+    .mutation(({ input, ctx }) => loginHandler({ input, ctx })),
+  logoutUser: t.procedure.mutation(({ ctx }) => logoutHandler({ ctx })),
+  refreshToken: t.procedure.query(({ ctx }) =>
+    refreshAccessTokenHandler({ ctx })
+  ),
+});
+
+const isAuthorized = t.middleware(({ ctx, next }) => {
+  if (!ctx.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to access this resource",
+    });
+  }
+  return next();
+});
+
+const isAuthorizedProcedure = t.procedure.use(isAuthorized);
+
+const userRouter = t.router({
   sayHello: t.procedure.query(async () => {
     const message = await redisClient.get("tRPC");
     return { message };
   }),
+  getMe: isAuthorizedProcedure.query(({ ctx }) => getMeHandler({ ctx })),
 });
+
+const appRouter = t.mergeRouters(authRouter, userRouter);
 
 export type AppRouter = typeof appRouter;
 
 const app = express();
 if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
+
+app.use(cookieParser());
 
 app.use(
   cors({
--- server/src/config/default.ts
+++ server/src/config/default.ts
@@ -1,11 +1,28 @@
 import path from "path";
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
-const customConfig: { port: number; origin: string; dbUri: string } = {
+const customConfig: {
+  port: number;
+  accessTokenExpiresIn: number;
+  refreshTokenExpiresIn: number;
+  origin: string;
+  dbUri: string;
+  accessTokenPrivateKey: string;
+  accessTokenPublicKey: string;
+  refreshTokenPrivateKey: string;
+  refreshTokenPublicKey: string;
+} = {
   port: 8000,
-  origin: process.env.ORIGIN as unknown as string,
+  origin: process.env.ORIGIN as string,
 
-  dbUri: process.env.MONGODB_URI as unknown as string,
+  dbUri: process.env.MONGODB_URI as string,
+
+  accessTokenExpiresIn: 15,
+  refreshTokenExpiresIn: 60,
+  accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY as string,
+  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY as string,
+  refreshTokenPrivateKey: process.env.REFRESH_TOKEN_PRIVATE_KEY as string,
+  refreshTokenPublicKey: process.env.REFRESH_TOKEN_PUBLIC_KEY as string,
 };
 
 export default customConfig;
